116e75a96ed643fe0631cee9d85078dd2416c78a
Jaheim Dazzell <bennyjaheim@gmail.com>
2025-12-19 00:33:59 -0400

    Finished?

--- js/logout.js
+++ js/logout.js
@@ -1,9 +1,4 @@
 document.getElementById("logout").addEventListener("click", e => {
     e.preventDefault();
-
-    fetch("php/logout.php", { method: "POST" })
-        .then(r => r.json())
-        .then(() => {
-            window.location.replace("login.html");
-        });
+    window.location.href = "php/logout.php";
 });
